68a66222558d0245f4fbb222d1e639b86eaee0cc
injae7034 <injae7034@gmail.com>
2022-05-19 21:05:01 +0900

    정렬하기 개선

---
+++ AddressBook/src/main/java/injae/AddressBook/personal/adapter/in/web/HomeForm.java
@@ -0,0 +1,12 @@
+package injae.AddressBook.personal.adapter.in.web;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class HomeForm {
+
+    private boolean isCheckBoxChecked;
+
+}
