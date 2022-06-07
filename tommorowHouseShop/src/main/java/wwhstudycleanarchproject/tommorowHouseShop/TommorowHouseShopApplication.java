285d3caf49e9969c39748d45027409c005e017ed
injae7034 <injae7034@gmail.com>
2022-06-07 15:28:04 +0900

    프로젝트 초기 생성

---
+++ tommorowHouseShop/src/main/java/wwhstudycleanarchproject/tommorowHouseShop/TommorowHouseShopApplication.java
@@ -0,0 +1,13 @@
+package wwhstudycleanarchproject.tommorowHouseShop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TommorowHouseShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TommorowHouseShopApplication.class, args);
+	}
+
+}
---
+++ tommorowHouseShop/src/test/java/wwhstudycleanarchproject/tommorowHouseShop/TommorowHouseShopApplicationTests.java
@@ -0,0 +1,13 @@
+package wwhstudycleanarchproject.tommorowHouseShop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TommorowHouseShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
