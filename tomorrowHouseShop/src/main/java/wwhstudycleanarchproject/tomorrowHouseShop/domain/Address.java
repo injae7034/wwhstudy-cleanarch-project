ad67553e5471fe8afd148d83d1e382c80c7b4bed
injae7034 <injae7034@gmail.com>
2022-06-08 19:04:30 +0900

    Buyer, Order, Address 클래스 생성

---
+++ tomorrowHouseShop/src/main/java/wwhstudycleanarchproject/tomorrowHouseShop/domain/Address.java
@@ -0,0 +1,23 @@
+package wwhstudycleanarchproject.tomorrowHouseShop.domain;
+
+import lombok.Getter;
+
+import javax.persistence.Embeddable;
+
+@Embeddable
+@Getter
+public class Address {
+
+    private String city;
+    private String street;
+    private String zipcode;
+
+    protected Address() {
+    }
+
+    public Address(String city, String street, String zipcode) {
+        this.city = city;
+        this.street = street;
+        this.zipcode = zipcode;
+    }
+}
---
+++ tomorrowHouseShop/src/main/java/wwhstudycleanarchproject/tomorrowHouseShop/domain/Order.java
@@ -0,0 +1,31 @@
+package wwhstudycleanarchproject.tomorrowHouseShop.domain;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import static javax.persistence.FetchType.LAZY;
+
+@Entity
+@Table(name = "orders")
+@Getter @Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Order {
+
+    @Id @GeneratedValue
+    @Column(name = "order_id")
+    private Long id;
+
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "buyer_id")
+    private Buyer buyer;
+
+}
