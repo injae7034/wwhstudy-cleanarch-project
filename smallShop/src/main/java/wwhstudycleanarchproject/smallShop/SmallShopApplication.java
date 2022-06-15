cac365e553edb2531ed613d940d3f26d425f733a
injae7034 <injae7034@gmail.com>
2022-06-15 09:55:20 +0900

    새 프로젝트 생성

---
+++ smallShop/src/main/java/wwhstudycleanarchproject/smallShop/SmallShopApplication.java
@@ -0,0 +1,13 @@
+package wwhstudycleanarchproject.smallShop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmallShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmallShopApplication.class, args);
+	}
+
+}
---
+++ smallShop/src/test/java/wwhstudycleanarchproject/smallShop/SmallShopApplicationTests.java
@@ -0,0 +1,13 @@
+package wwhstudycleanarchproject.smallShop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmallShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
