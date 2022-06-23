c91da490bbf601e16ffc755bb7aa970bd2d659bc
injae7034 <injae7034@gmail.com>
2022-06-23 13:49:40 +0900

    회원가입 화면 및 컨트롤러 구현

--- productManagementSystem/src/main/java/wwhstudyCleanarchProject/productManagementSystem/Member/adapter/in/web/RegisterMemberController.java
+++ productManagementSystem/src/main/java/wwhstudyCleanarchProject/productManagementSystem/Member/adapter/in/web/RegisterMemberController.java
@@ -2,11 +2,15 @@ package wwhstudyCleanarchProject.productManagementSystem.Member.adapter.in.web;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import wwhstudyCleanarchProject.productManagementSystem.Member.application.port.in.RegisterMemberUseCase;
 import wwhstudyCleanarchProject.productManagementSystem.Member.domain.Member;
+
+import javax.validation.Valid;
 
 @Controller
 @RequiredArgsConstructor
@@ -18,5 +22,16 @@ public class RegisterMemberController {
     @GetMapping("/register")
     public String registerForm(@ModelAttribute("member") Member member) {
         return "members/registerMemberForm";
+    }
+
+    @PostMapping("/register")
+    public String registerMember(@Valid @ModelAttribute Member member, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return "members/registerMemberForm";
+        }
+
+        useCase.registerMember(member);
+
+        return "redirect:/";
     }
 }
