c4ed41814fdbe3d6fb4d82a9fb18b75af3184221
injae7034 <injae7034@gmail.com>
2022-07-26 16:11:01 +0900

    불필요한 import 제거

--- AddressBook/src/main/java/injae/AddressBook/member/adapter/in/web/logout/LogoutMemberController.java
+++ AddressBook/src/main/java/injae/AddressBook/member/adapter/in/web/logout/LogoutMemberController.java
@@ -4,9 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 @Controller
--- AddressBook/src/main/java/injae/AddressBook/member/adapter/in/web/withdrawal/WithdrawalMemberController.java
+++ AddressBook/src/main/java/injae/AddressBook/member/adapter/in/web/withdrawal/WithdrawalMemberController.java
@@ -3,18 +3,13 @@ package injae.AddressBook.member.adapter.in.web.withdrawal;
 import injae.AddressBook.member.application.port.in.FindMemberQuery;
 import injae.AddressBook.member.application.port.in.WithdrawalMemberUseCase;
 import injae.AddressBook.member.domain.Member;
-import injae.AddressBook.personal.application.port.in.ErasePersonalUseCase;
-import injae.AddressBook.personal.application.port.in.GetPersonalsQuery;
-import injae.AddressBook.personal.domain.Personal;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.SessionAttribute;
 
-import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 @Controller
