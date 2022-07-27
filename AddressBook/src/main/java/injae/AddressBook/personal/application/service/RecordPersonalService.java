5b41594470612020ccfe2eaa361886135c8104c5
injae7034 <injae7034@gmail.com>
2022-07-27 18:23:46 +0900

    Update RecordPersonalService.java

--- AddressBook/src/main/java/injae/AddressBook/personal/application/service/RecordPersonalService.java
+++ AddressBook/src/main/java/injae/AddressBook/personal/application/service/RecordPersonalService.java
@@ -22,18 +22,25 @@ public class RecordPersonalService implements RecordPersonalUseCase {
     public Long recordPersonal(String name, String address, String telephoneNumber,
                                String emailAddress, Member member) {
 
+        //db에 저장할 새로운 personal 객체 생성
         Personal personal = new Personal(name, address, telephoneNumber,
                 emailAddress, member);
 
+        //db에 저장(personal은 member의 참조값을 알고 있음)
         recordRepository.save(personal);
 
+        //member는 현재 personal의 참조값을 모르기 때문에 연관관계를 맺어줘야 함.
+        // 멤버를 찾음으로써 영속성 컨텍스트를 활성화시킨다.
         Member findMember = findRepository.findByEmail(member.getEmail()).orElse(null);
 
+        //이메일에 해당하는 멤버가 있으면
         if (findMember != null) {
+            //멤버에 새로 생성한 personal의 참조값을 추가한다.
             findMember.getPersonals().add(personal);
 
             return personal.getId();
         } else {
+            //멤버가 없으면 예외를 발생시킨다.
             throw new IllegalStateException();
         }
     }
