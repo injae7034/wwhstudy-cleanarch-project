0e337cf2080e1419ccf7e4113037906d847f76df
injae7034 <injae7034@gmail.com>
2022-08-01 16:36:04 +0900

    login api get메소드에서 post메소드로 수정

--- AddressBook/src/main/java/injae/AddressBook/member/adapter/in/api/login/LoginMemberApiController.java
+++ AddressBook/src/main/java/injae/AddressBook/member/adapter/in/api/login/LoginMemberApiController.java
@@ -6,7 +6,7 @@ import injae.AddressBook.member.domain.Member;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,7 +18,7 @@ public class LoginMemberApiController {
 
     private final LoginMemberUseCase useCase;
 
-    @GetMapping("/members/login")
+    @PostMapping("/members/login")
     public ResponseEntity<LoginMemberResponse> loginMember(@RequestBody @Valid LoginMemberRequest request) {
 
         Member loginMember = useCase.loginMember(request.getEmail(), request.getPassword());
