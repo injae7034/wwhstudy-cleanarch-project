d53e09c751b50329f143feab91e1ca22ff06d01c
injae7034 <injae7034@gmail.com>
2022-08-02 19:18:32 +0900

    Update CorrectPersonalByMemberApiController.java

--- AddressBook/src/main/java/injae/AddressBook/personal/adapter/in/api/correct/CorrectPersonalByMemberApiController.java
+++ AddressBook/src/main/java/injae/AddressBook/personal/adapter/in/api/correct/CorrectPersonalByMemberApiController.java
@@ -8,15 +8,12 @@ import injae.AddressBook.personal.application.port.in.GetPersonalQuery;
 import injae.AddressBook.personal.domain.Personal;
 import injae.AddressBook.personal.exception.PersonalNotFoundException;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import javax.validation.Valid;
-import java.net.URI;
 
 @RestController
 @RequiredArgsConstructor
