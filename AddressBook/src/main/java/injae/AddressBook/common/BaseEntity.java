06136410a10fd8387e63ef94890dce00c7643ef9
Injae Park <ijpark@autocrypt.io>
2022-10-06 10:22:48 +0900

    Create BaseEntity.java

---
+++ AddressBook/src/main/java/injae/AddressBook/common/BaseEntity.java
@@ -0,0 +1,29 @@
+package injae.AddressBook.common;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.Column;
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseEntity {
+
+    @CreatedDate
+    @Column(updatable = false)
+    private LocalDateTime createdDate = LocalDateTime.now();
+
+    @LastModifiedDate
+    private LocalDateTime lastModifiedDate = LocalDateTime.now();
+
+}
