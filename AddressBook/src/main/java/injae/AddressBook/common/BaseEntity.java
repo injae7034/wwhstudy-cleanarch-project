f5cf3abfffd9a8e88e5beeb8eaf2b285fac5cfc9
Injae Park <ijpark@autocrypt.io>
2022-10-06 10:24:08 +0900

    Update BaseEntity.java

--- AddressBook/src/main/java/injae/AddressBook/common/BaseEntity.java
+++ AddressBook/src/main/java/injae/AddressBook/common/BaseEntity.java
@@ -2,9 +2,7 @@ package injae.AddressBook.common;
 
 import lombok.Getter;
 import lombok.Setter;
-import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedBy;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
